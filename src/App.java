91cd4e3ba491f908b5e6cda8e742a25b633a5d8a
Kovács Dorina <kovacsdorina2@gmail.com>
2022-04-06 12:15:37 +0200

    Kezdés

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Tama verzió: 0.0.1");
+        Tama tama = new Tama("Tama");
+        tama.ertelmezo();
+    }
+}
